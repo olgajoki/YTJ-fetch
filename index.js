d8a824dc5562401e07d3aa80136b329eafafdbc7
olgajoki <olga.jokinen@hotmail.com>
2023-05-08 14:05:49 +0300

    for-->map

--- index.js
+++ index.js
@@ -1,62 +1,70 @@
-
-const { json } = require('body-parser');
+//const { json } = require('body-parser');
 const express = require('express')
 const app = express()
 
 app.use(express.json())
 
 let newData = [];   
-let apiFetch;
-const apiurl = "http://avoindata.prh.fi/bis/v1/?businessId=";
-let arrayLength;
+const apiurl = 'http://avoindata.prh.fi/bis/v1/?businessId=';
+
 
 createURL();
 
 function createURL() {
 
     let dataToAPI = [
-    //'1532726-8',
+   // '1532726-8',
    // '1891976-0',
-    '2083639-0',
+   // '2083639-0',
     '2432892-4',
-   // '0127485-5',
+    '0127485-5',
     '0974176-1',
     '2544336-9',
     '2399345-5',
     '0124461-3',
     '0203107-0'
-    ]
+    ];
 
-    for(let i = 0; i < dataToAPI.length; i++){
-        let businessID = dataToAPI[i];   
 
-        apiFetch = apiurl + businessID;
-        arrayLength = dataToAPI.length; 
-        console.log(apiFetch)
-        fetchAPI(apiFetch);                                  
+    dataToAPI.map(combineURL);
 
+    function combineURL(ID) {
+
+      console.log(apiurl + ID)
+        return fetchAPI((apiurl + ID));
     }
 
-    function fetchAPI(fetchAPI){
+    function fetchAPI(apiFetch){
+    //  setTimeout(function(){
+    //  }, 5000);
+        fetch(apiFetch).then(function(response) {
+          console.log(response)
 
-
-        fetch(fetchAPI).then(function(response) {
-          //  console.log(response)
+      /*    try {
+            const myObject = JSON.parse(apiFetch);
+          } catch (error) {
+            if (error instanceof SyntaxError) {
+              console.error('Invalid JSON:', error.message);
+            } else {
+              throw error;
+            }
+          }*/
             return response.json();
         }).then(function(data) {
-            console.log(data)
+            //console.log(data)
             showResults(data)            				
-        }).catch(function(error){          
+        }).catch(function(error){    
             console.log(error);            
         });
     }
+
 
     function showResults(jsonData) {
         
    // console.log(jsonData) 
     newData.push(jsonData.results[0].businessId);
     newData.push(jsonData.results[0].registedOffices[0].name);
-//    newData.push(jsonData.results[0].addresses[0].street + " " + jsonData.results[0].addresses[0].postCode + " " + jsonData.results[0].addresses[0].city)
+    //    newData.push(jsonData.results[0].addresses[0].street + " " + jsonData.results[0].addresses[0].postCode + " " + jsonData.results[0].addresses[0].city)
 
     }
 
@@ -64,6 +72,8 @@ function createURL() {
 
   
   app.get('/', (req, res) => {
+   // res.end(JSON.stringify(newData))
+
     res.json(newData)
   })
   
