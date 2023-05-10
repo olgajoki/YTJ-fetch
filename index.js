f9aea5dbc49b98a9ac179f7d18adb1fd4e95fcdc
olgajoki <olga.jokinen@hotmail.com>
2023-05-10 10:37:56 +0300

    get apis with setInterval

--- index.js
+++ index.js
@@ -1,83 +1,73 @@
-//const { json } = require('body-parser');
+const { json } = require('body-parser');
 const express = require('express')
 const app = express()
 
 app.use(express.json())
 
-let newData = [];   
+let newData = [];
+let wholeURL;
+let array = [];
 const apiurl = 'http://avoindata.prh.fi/bis/v1/?businessId=';
 
+const dataToAPI = [
+  '1532726-8',
+  '1891976-0',
+  '2083639-0',
+  '2432892-4',
+  '0127485-5',
+  '0974176-1',
+  '2544336-9',
+  '2399345-5',
+  '0124461-3',
+  '0203107-0'
+  ];
 
-createURL();
+  //combine 
+function combineURL(ID) {
+  wholeURL = apiurl+ID;
+  return wholeURL;
+}
 
-function createURL() {
+let urls = dataToAPI.map(combineURL);
+let i = 0;
+  setInterval( () => {
+    console.log("ennen fetchia")
+      fetchAPI(urls[i])
+    i++;
+    console.log("here")
+  }, 200);
 
-    let dataToAPI = [
-   // '1532726-8',
-   // '1891976-0',
-   // '2083639-0',
-    '2432892-4',
-    '0127485-5',
-    '0974176-1',
-    '2544336-9',
-    '2399345-5',
-    '0124461-3',
-    '0203107-0'
-    ];
-
-
-    dataToAPI.map(combineURL);
-
-    function combineURL(ID) {
-
-      console.log(apiurl + ID)
-        return fetchAPI((apiurl + ID));
-    }
-
-    function fetchAPI(apiFetch){
-    //  setTimeout(function(){
-    //  }, 5000);
-        fetch(apiFetch).then(function(response) {
-          console.log(response)
-
-      /*    try {
-            const myObject = JSON.parse(apiFetch);
-          } catch (error) {
-            if (error instanceof SyntaxError) {
-              console.error('Invalid JSON:', error.message);
-            } else {
-              throw error;
-            }
-          }*/
-            return response.json();
-        }).then(function(data) {
-            //console.log(data)
-            showResults(data)            				
-        }).catch(function(error){    
-            console.log(error);            
-        });
-    }
-
-
-    function showResults(jsonData) {
-        
-   // console.log(jsonData) 
-    newData.push(jsonData.results[0].businessId);
-    newData.push(jsonData.results[0].registedOffices[0].name);
-    //    newData.push(jsonData.results[0].addresses[0].street + " " + jsonData.results[0].addresses[0].postCode + " " + jsonData.results[0].addresses[0].city)
-
-    }
+    
+function fetchAPI(apiFetch){
+    fetch(apiFetch).then(function(response) {
+        return response.json();
+    }).then(function(data) {
+      console.log(data)
+        showResults(data)            				
+    }).catch(function(error){    
+        console.log(error);            
+    });
 
 }
 
-  
-  app.get('/', (req, res) => {
-   // res.end(JSON.stringify(newData))
+function showResults(jsonData) {   
+// console.log(jsonData) 
+newData.push(jsonData.results[0].businessId);
+newData.push(jsonData.results[0].registedOffices[0].name);
+//    newData.push(jsonData.results[0].addresses[0].street + " " + jsonData.results[0].addresses[0].postCode + " " + jsonData.results[0].addresses[0].city)
+}
 
-    res.json(newData)
-  })
-  
-  const PORT = 3001
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
+app.get('/', (req, res) => {
+res.json(newData)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+console.log(`Server running on port ${PORT}`)
+})
+
+  // tee lopetus ettei jatka pyörittämistä
+  // testaa millä ms etenee
+  //lataa kirjasto json to excel
+  // luo array itemeistä objektit
+  // write to file lopussa
